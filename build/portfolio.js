6aaba40231ed0fc5aa7200d40c5cbcdf49e02a8c
MateusArenas <MateusArenas97@gmail.com>
2022-05-26 17:58:52 -0300

    all

---
+++ build/portfolio.js
@@ -0,0 +1,26 @@
+const { buildHTML, useRequireComponents } = require('../lib')
+
+useRequireComponents('.html')
+
+const { List } = require('../lib/components')
+
+const Header = require('../components/header.html')
+const Profile = require('../components/profile.html')
+
+const Projects = require('../components/projects/index.html')
+const ProjectSection = require('../components/projects/section.html')
+const ProjectItem = require('../components/projects/item.html')
+
+const Index = require("../screens/index.html")
+
+buildHTML('./portfolio.html', Index({ 
+    header: Header({}),
+    profile: Profile(require('../data/profile.json')),
+    projects: List({ 
+        component: Projects, 
+        section: ProjectSection,
+        colluns: 3,
+        data: require('../data/projects.json'),
+        item: ProjectItem 
+    }), 
+}))
