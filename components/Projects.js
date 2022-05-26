8420e9c15db7efd8a18bf6b54a1c240c498c917f
MateusArenas <MateusArenas97@gmail.com>
2022-05-26 02:09:39 -0300

    first commit

--- components/Projects.js
+++ components/Projects.js
@@ -1,9 +1,9 @@
 var Projects = async () => {
-    const multidata = await getData("../data/projects.json")
+    const multidata = await getData("./data/projects.json")
 
-    const list = await require("../templates/projects/index.html")
-    const section = await require("../templates/projects/section.html")
-    const item = await require("../templates/projects/item.html")
+    const list = await require("./templates/projects/index.html")
+    const section = await require("./templates/projects/section.html")
+    const item = await require("./templates/projects/item.html")
 
     const childrens = multidata.map(data => data.map(props => buildComponent(item, { ...props })))
     
