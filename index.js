3369a8b6393176a7478161b8d3637349ab364e20
MateusArenas <MateusArenas97@gmail.com>
2022-05-26 03:29:51 -0300

    first commit

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
 const Projects = require('./components/Projects')
 const Courses = require('./components/Courses')
 
+const projects = require("./data/projects.json")
+const courses = require("./data/courses.json")
+
 $(document).ready(async function() {
 
-    $("#about").append(await Projects({ data: await required("./data/projects.json") }))
-    $("#about").append(await Courses({ data: await required("./data/courses.json") }))
+    $("#about").append(await Projects({ data: projects }))
+    $("#about").append(await Courses({ data: courses }))
   
 });
