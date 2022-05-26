9e1c945ae051e53e3ba72cefd602b750c0a056b2
MateusArenas <MateusArenas97@gmail.com>
2022-05-26 02:03:56 -0300

    first commit

---
+++ components/Projects.js
@@ -0,0 +1,17 @@
+async function Projects () {
+    const multidata = await getData("./data/projects.json")
+
+    const list = await require("./templates/projects/index.html")
+    const section = await require("./templates/projects/section.html")
+    const item = await require("./templates/projects/item.html")
+
+    const childrens = multidata.map(data => data.map(props => buildComponent(item, { ...props })))
+    
+    const children = childrens.map(props => buildComponent(section, { ...props })).join('')
+
+    const courses = await buildComponent(list, { children });
+
+    $("#about").append(courses)
+}
+
+module.exports = Projects
