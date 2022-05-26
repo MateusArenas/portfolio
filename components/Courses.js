756c69899bc535da8f4faa9224877b6a9f2525db
MateusArenas <MateusArenas97@gmail.com>
2022-05-26 02:59:11 -0300

    first commit

--- components/Courses.js
+++ components/Courses.js
@@ -1,13 +1,11 @@
-(async () => {
+async function Courses () {
     const data = await require("./data/courses.json")
 
     const CourseList = await require("./templates/courses/index.html")
 
     const CourseItem = await require("./templates/courses/item.html")
     
-    var Courses = () => {
-        const children = data.map(props => CourseItem(props))
-        
-        return CourseList({ children });
-    }
-})()
+    return CourseList({ 
+        children: data.map(props => CourseItem(props)) 
+    });
+}
--- components/Projects.js
+++ components/Projects.js
@@ -1,16 +1,13 @@
-(async () => {
+async function Projects () {
     const multidata = await require("./data/projects.json")
     
     const ProjectList = await require("./templates/projects/index.html")
     const ProjectSection = await require("./templates/projects/section.html")
     const ProjectItem = await require("./templates/projects/item.html")
-
-    var Projects = () => {
     
-        const childrens = multidata.map(data => data.map(props => ProjectItem({ ...props })))
-        
-        const children = childrens.map(props => ProjectSection({ ...props }))
+    const childrens = multidata.map(data => data.map(props => ProjectItem({ ...props })))
     
-        return ProjectList({ children });
-    }
-})()
+    return ProjectList({ 
+        children: childrens.map(props => ProjectSection({ ...props }))
+    });
+}
