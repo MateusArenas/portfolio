aedfdc6305d922a345721080e11acef11367711c
MateusArenas <MateusArenas97@gmail.com>
2022-05-26 17:12:53 -0300

    all

--- run.js
+++ run.js
@@ -4,6 +4,9 @@ useRequireComponents('.html')
 
 const { List } = require('./lib/components')
 
+const Experiences = require('./components/experiences/index.html')
+const ExperienceItem = require('./components/experiences/item.html')
+
 const Courses = require('./components/courses/index.html')
 const CourseItem = require('./components/courses/item.html')
 
@@ -11,11 +14,14 @@ const Projects = require('./components/projects/index.html')
 const ProjectSection = require('./components/projects/section.html')
 const ProjectItem = require('./components/projects/item.html')
 
-
 const App = require("./components/app.html")
 
-
 buildHTML('./index.html', App({ 
+    experiences: List({ 
+        component: Experiences, 
+        data: require('./data/experiences.json'),
+        item: ExperienceItem 
+    }),
     projects: List({ 
         component: Projects, 
         section: ProjectSection,
